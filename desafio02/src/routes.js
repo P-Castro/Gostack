3dd1f045d2832669890688bf834777c9670375e0
Pedro Castro <peeeedro_h@hotmail.com>
2019-10-24 20:58:58 -0300

    Cofiguração inicial do desafio02

--- desafio02/src/routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'; // usado para definir as rotas
-
-const routes = new Router();
-
-routes.get('/', (req, res) => res.json({ message: 'Hellor World' }));
-
-export default routes;
